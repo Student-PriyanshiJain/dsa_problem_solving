27e700eb19bcfb324e5a8ad50de38bdd94b16d0e
Student-PriyanshiJain <priynshijain1572@gmail.com>
2024-02-16 11:03:00 +0530

    Added P4 solution

--- src/Two_Pointers/P4.java
+++ src/Two_Pointers/P4.java
@@ -1,9 +1,27 @@
 package Two_Pointers;
 
+import java.util.Arrays;
+
 public class P4 {
 
-    // (gfg) :- Find all triplets with zero sum -
-    public static void main(String[] args) {
+    public boolean findTriplets(int arr[] , int n){
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            int start = i+1;
+            int end = n-1;
 
+            while(start<end){
+                if(arr[i]+arr[start]+arr[end]==0){
+                    return true;
+                }
+                else if(arr[i]+arr[start]+arr[end]<0){
+                    start++;
+                }
+                else{
+                    end--;
+                }
+            }
+        }
+        return false;
     }
 }
